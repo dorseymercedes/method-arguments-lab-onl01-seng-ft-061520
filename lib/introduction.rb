65ce243f80f539479828d4fa6aa6ee2799831fde
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-06-16 14:31:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,4 +8,4 @@ introduction(Josh)
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in  #{language}."
   
-  introduction_with_language(Dan, Ember)
+  introduction_with_language(Dan, Ember.js)
